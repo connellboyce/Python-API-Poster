607f66d5e3e7dcb1f0717844792dc2a72588b9c0
Connell Boyce <connell.p.boyce@gmail.com>
2020-06-09 13:42:49 -0400

    Updating README and code cleanup

--- PythonPoster.py
+++ PythonPoster.py
@@ -1,18 +1,28 @@
 import requests
 import json
 
+# Set URL to post to and declare authorization header
+# Auth header will expire and need to be replaced after some time
 url = 'http://localhost:9999/api/pepper/add'
 headers = {'Authorization' : 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjb25uZWxsIiwiaWF0IjoxNTkxNzA5MzkwLCJleHAiOjE1OTE3NjkzOTB9.02FykKqnRJ6QZRKuMarIhkPjkJeX4zIT5rgSWmf-bstwPF39XWzNxBJ373lBvUPhrn9M1AOs8P7sc-5LxQsryA', 'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+fileName = 'pepperList.json'
+arrayName = 'peppers'
 
-with open('pepperList.json') as data_file:
+# Open pepperList.json to iterate through objects
+with open(fileName) as data_file:
     json_file = json.load(data_file)
-    for pepper in json_file['peppers']:
-        temp = json.dumps(pepper)
-        # temp = str(pepper).replace('"','###').replace("'", '"').replace("###",'"')
-        print(temp)
-        r = requests.post(url, data=temp, headers=headers)
-        print(r)
 
-#json = '{"name" : "Devil Tongue","species" : "chinense","origin" : "Pennsylvania","shuMin" : "125,000","shuMax" : "325,000","imageURL" : "http://www.chileplanet.eu/schede/pepper/Devil%20Tongue.jpg","description" : "Devil Tongue: C. chinense. Variety from Pennsylvania. Its exact origins are unknown. It a kind of chili closely related to Habanero. It is believed to have originated from an Amish farmer who discovered him few decades ago amongst others grow Habanero. The fruits are similar to the variety Fatalii but with a skin smoother and more compact. Mature from green to red or golden yellow depending on the variety. 125.000 - 325.000 SHU."}'
-#r = requests.post(url, data=json, headers=headers)
-#print(r)
+    # For each pepper
+    for arrayItem in json_file[arrayName]:
+
+        # Convert to json form
+        temp = json.dumps(arrayItem)
+
+        # Print json object to console
+        print(temp)
+
+        # Send a post request to the url with the specified json as data and with the appropriate headers
+        r = requests.post(url, data=temp, headers=headers)
+
+        # Print response code to console
+        print(r)
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Python-API-Poster
+# Python API Poster
 Simple python script to post contents of a JSON file to a RESTful API
+
+### Table of Contents
+1. [Installation](#Installation)
+2. [Necessary Components](#Necessary-Components)
+2. [Connecting to a Different Database](#Connecting-to-a-Different-Database)
+
+
+### **Installation**
+Clone the repository.
+```bash
+$ git clone https://github.com/connellboyce/Python-API-Poster.git
+```
+
+Open the program in your IDE and run.
+
+
+### **Necessary Components**
+* Python 3
+* POST Permissions in a web API
+
+
+### **Connecting to a Different Database**
+This program is specialized to write to the database for [my Pepper Garden app](https://github.com/connellboyce/pepper-garden), but this can be changed.
+There are a few things you will need to have set up first, however.
+1. You must have an API allowing POST requests
+2. You must have a JSON file containing an array of objects
+3. The JSON objects must be valid in the eyes of the desired API
+    * Do not try to use a JSON object lacking required fields or having information the API will reject
+4. Replace the variables at the top of the Python script to match the API you would like to post to
+    * url: This should be the API URL which allows POST requests
+    * headers: This should include any HTTP headers that the API requires. Authorization headers, like the one in the variable currently, will usually expire. Make sure your headers are valid.
+    * fileName: Put the name of the JSON file you would like to iterate through and POST in the directory here. The [pepperList.json](pepperList.json) file is in the correct location for reference.
+    * arrayName: This should be the name of the array contained inside the JSON file.
+5. Run the program
+
